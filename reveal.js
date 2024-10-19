1acc372da2ad776c54f0e994fe8b2158218c3345
advyfthn. <141932647+voca12@users.noreply.github.com>
2024-10-19 21:47:56 +0700

    Delete reveal.js

--- reveal.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("revealName").addEventListener("click", function() {
-    var name = prompt("Happy Birthday, Ibuk!:");
-    if (name) {
-        document.getElementById("name").textContent = Ibuk + " 🎉";
-        document.getElementById("name").style.display = "block";
-        document.getElementById("revealName").style.display = "Ibuk";
-    }
-});
